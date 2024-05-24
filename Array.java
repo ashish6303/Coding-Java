fe9dcf0854eaa7013ccadf2624c7cbf88254eac1
ashish6303 <ashish977194@gmail.com>
2024-05-24 23:47:37 +0530

    Static array

---
+++ Array.java
@@ -0,0 +1,21 @@
+public class Array {
+
+    public static int staticSum(int [] num)
+    {
+        int sum = 0;
+        for (int j : num) {
+            sum += j;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+//        This is static array creation
+        int [] num = {1,2,3,4,5};
+
+//        This is dynamic array creation
+
+
+        System.out.println("sum is : " + staticSum(num));
+    }
+}
